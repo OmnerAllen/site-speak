328f3e388261cdf6a0bf42000d8b3240c998e28b
OmnerAllen <bookwormno2@gmail.com>
2026-03-23 17:28:17 -0600

    maybe this is important, git is weird with our custom forms

---
+++ client/src/components/SmallTextInput.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+export interface SmallTextInputProps extends React.InputHTMLAttributes<HTMLInputElement> {
+  label: string;
+}
+
+export const SmallTextInput: React.FC<SmallTextInputProps> = ({ label, ...props }) => {
+  return (
+    <div className="flex flex-col gap-1">
+      <label className="text-sm font-medium text-brick-300">{label}</label>
+      <input
+        type="text"
+        className="px-3 py-2 bg-brick-100 text-brick-900 border border-brick-300 rounded-md focus:outline-none focus:ring-2 focus:ring-brick-500"
+        {...props}
+      />
+    </div>
+  );
+};
